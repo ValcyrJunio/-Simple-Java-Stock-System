966acfec94a9aa2ce62d8050794f8af307982dcf
Valcyr Junior <vieiravalcyr@gmail.com>
2026-06-10 10:09:59 -0300

    Update simpleStock.java
    added search by product name

--- simpleStock.java
+++ simpleStock.java
@@ -1,18 +1,21 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 
 public class simpleStock {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int position = 0;
         int option = 0;
         int total = 0;
         int [] quantity = new int[15];
         String [] products = new String[15];
 
-        while (option != 6){
+        while (option != 7){
+            boolean found = false;
             System.out.println("=== INVENTORY SYSTEM === ");
-            System.out.println("1 - Add product \n 2 - Remove a product \n 3 - List all products \n 4 - Add quantity \n 5 - Remove quantity \n 6 - Leave  ");
+            System.out.println("1 - Add product \n 2 - Remove a product \n 3 - List all products \n 4 - Add quantity \n 5 - Remove quantity \n 6 - Search for a product \n  7 - Leave  ");
             option = scan.nextInt();
             scan.nextLine();
 
@@ -76,8 +79,19 @@ public class simpleStock {
                     } else {
                         System.out.println("Type a valid position!");
                     }
-                    continue;
 
+                case 6:
+                    System.out.println("Type the name of the product that you are searching for: ");
+                    String name = scan.nextLine();
+                    for (int i = 0 ; i < total ; i++){
+                        if (products[i].toLowerCase().contains(name.toLowerCase())){
+                            System.out.printf("[Id = %d | Product = %s | Quantity = %d] \n", i, products[i], quantity[i]);
+                            found = true;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("ERROR - CAN'T FIND THE PRODUCT");
+                    }
             }
 
         }
